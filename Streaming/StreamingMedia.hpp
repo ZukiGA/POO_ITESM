0abffd029e9bc6dfed9b6ae2e21c8bb57b4a8d3b
BryanGAA <49618395+ZukiGA@users.noreply.github.com>
2020-06-05 18:18:42 -0500

    lol

--- Streaming/StreamingMedia.hpp
+++ Streaming/StreamingMedia.hpp
@@ -24,6 +24,44 @@ StreamingMedia::StreamingMedia(){
 void StreamingMedia::on(){
 	cout << "Welcome" << endl;
 	cout << "Choose one of the next options:" << endl;
+
+	int eleccion = 0;
+
+	cout << endl << "-----MENÚ-----" << endl << "Escoge una opción." << endl << "1. Ver vuelos" << endl << "2. Agregar vuelo" << endl
+	<< "3. Mostrar vuelos de una fecha específica" << endl << "4. Actualizar vuelo retrasado" << endl << "5. Cargar vuelos desde archivo"
+	<< endl << "6. Guardar vuelos en archivo" << endl << "0. Salir" << endl;
+
+	cin >> eleccion;
+
+	//return eleccion;
+
+	eleccionUsuario = menuPrincipal();
+
+	while (eleccionUsuario>0&&eleccionUsuario<7) {
+		if (eleccionUsuario == 1) {
+			verVuelo(datosVuelos);
+		} else {
+			if (eleccionUsuario == 2) {
+				datosVuelos = agregarVuelo(datosVuelos);
+			} else {
+				if (eleccionUsuario == 3) {
+					mostrarVuelo(datosVuelos);
+				} else {
+					if (eleccionUsuario == 4) {
+						datosVuelos = actualizarVuelo(datosVuelos);
+					} else {
+						if (eleccionUsuario == 5) {
+							datosVuelos = cargarVuelo(datosVuelos);
+						} else {
+							if (eleccionUsuario == 6)
+								guardarVuelo(datosVuelos);
+						}
+					}
+				}
+			}
+		}
+		eleccionUsuario = menuPrincipal();
+	}
 }
 
 void StreamingMedia::off(){

b63815fa1f50e4ca3a885e2aced9ca16e768e041
BryanGA <49618395+ZukiGA@users.noreply.github.com>
2020-06-04 23:41:31 -0500

    ver2

---
+++ Streaming/StreamingMedia.hpp
@@ -0,0 +1,36 @@
+/*
+Author: Bryan González Arellano
+ID : A01734198
+Creation date: 06/04/20
+Modification date: 06/04/20
+File: StreamingMedia.h
+*/
+
+class StreamingMedia {
+private:
+public:
+	StreamingMedia();
+	void on();
+	void off();
+	void loadInfo();
+	void search();
+};
+
+StreamingMedia::StreamingMedia(){
+}
+
+void StreamingMedia::on(){
+
+}
+
+void StreamingMedia::off(){
+
+}
+
+void StreamingMedia::loadInfo(){
+
+}
+
+void StreamingMedia::search(){
+	
+}
---
+++ Streaming/streamingForFun.cpp
@@ -0,0 +1,20 @@
+
+/*
+Author: Bryan González Arellano
+ID : A01734198
+Creation date: 06/04/20
+Modification date: 06/04/20
+// File: streamingForFun.cpp
+*/
+
+#include <bits/stdc++.h>
+#include "StreamingMedia.hpp"
+
+using namespace std;
+
+int main() {
+	StreamingMedia myStreamingMedia;
+	myStreamingMedia.on();
+	myStreamingMedia.off();
+	return 0;
+}
